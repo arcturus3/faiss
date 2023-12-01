66a747e5d523eefb133f609c4c994c4a9b4c69ce
Arti Schmidt <artischmidt3@gmail.com>
2023-12-01 01:11:32 -0500

    Modify scikit-learn mean shift demo for use with faiss

--- report/plot_mean_shift.py
+++ report/plot_mean_shift.py
@@ -16,11 +16,13 @@ import numpy as np
 from sklearn.cluster import MeanShift, estimate_bandwidth
 from sklearn.datasets import make_blobs
 
+import faiss
+
 # %%
 # Generate sample data
 # --------------------
 centers = [[1, 1], [-1, -1], [1, -1]]
-X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.6)
+X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.4)
 
 # %%
 # Compute clustering with MeanShift
@@ -29,10 +31,19 @@ X, _ = make_blobs(n_samples=10000, centers=centers, cluster_std=0.6)
 # The following bandwidth can be automatically detected using
 bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
 
-ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-ms.fit(X)
-labels = ms.labels_
-cluster_centers = ms.cluster_centers_
+X = X.astype(np.float32)
+n, d = X.shape
+X_ptr = faiss.swig_ptr(np.ravel(X))
+
+ms = faiss.MeanShiftClustering(d, n, X_ptr)
+ms.bandwidth = bandwidth
+ms.train()
+ms.connected_components()
+
+Y = np.reshape(faiss.rev_swig_ptr(ms.ys, n * d), (n, d))
+labels = faiss.rev_swig_ptr(ms.labels, n)
+centroids = faiss.vector_to_array(ms.centroids)
+cluster_centers = np.array([Y[i] for i in centroids])
 
 labels_unique = np.unique(labels)
 n_clusters_ = len(labels_unique)
@@ -47,8 +58,8 @@ import matplotlib.pyplot as plt
 plt.figure(1)
 plt.clf()
 
-colors = ["#dede00", "#377eb8", "#f781bf"]
-markers = ["x", "o", "^"]
+colors = ["#dede00", "#377eb8", "#f781bf", "#0000ff", "#ff0000", "#00ff00"]
+markers = ["x", "o", "^", "+", "+", "+"]
 
 for k, col in zip(range(n_clusters_), colors):
     my_members = labels == k
