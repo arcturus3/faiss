d252cec4b71556a1c74411eb176d774d74037248
Arti Schmidt <artischmidt3@gmail.com>
2023-12-01 01:18:58 -0500

    Add flag to control use of weighted BMS variant

--- faiss/MeanShiftClustering.cpp
+++ faiss/MeanShiftClustering.cpp
@@ -31,15 +31,15 @@ namespace faiss {
 MeanShiftClustering::MeanShiftClustering(size_t d, idx_t n, float* xs) : d(d), n(n), xs(xs) {
     ys = new float[n * d];
     ys1 = new float[n * d];
-    yst = new float[n * d];
-    weights = new float[d];
+    if (weighted) {
+        yst = new float[n * d];
+        weights = new float[d];
+    }
     labels = new size_t[n];
     index = IndexFlatL2(d);
 }
 
 float k(float x) {
-    // return x <= 1. ? 1. : 0.;
-    // return exp(-x * x);
     return exp(-x);
 }
 
@@ -69,26 +69,33 @@ void MeanShiftClustering::connected_components() {
 
 void MeanShiftClustering::train() {
     memcpy(ys, xs, n * d * sizeof(float));
-    for (size_t l = 0; l < d; l++)  weights[l] = 1. / d;
+    if (weighted) {
+        for (size_t l = 0; l < d; l++)  weights[l] = 1. / d;
+    }
     bool converged = false;
 
-    int it = 0;
-    while (!converged && it < 30) {
-        it++;
-        for (idx_t i = 0; i < n; i++) {
-            for (size_t l = 0; l < d; l++) {
-                yst[i * d + l] = sqrt(weights[l]) * ys[i * d + l];
+    size_t it = 0;
+    while (!converged) {
+        if (weighted) {
+            for (idx_t i = 0; i < n; i++) {
+                for (size_t l = 0; l < d; l++) {
+                    yst[i * d + l] = sqrt(weights[l]) * ys[i * d + l];
+                }
             }
         }
+        printf("iteration %zu\n", it);
+        it++;
         index.reset();
-        index.add(n, yst);
+        index.add(n, ys);
         RangeSearchResult search_result(n);
-        index.range_search(n, yst, HUGE_VAL, &search_result);
+        float limit = 3 * bandwidth; // HUGE_VAL for no limit
+        index.range_search(n, ys, limit, &search_result);
 
-        memset(weights, 0, d * sizeof(float));
+        if (weighted) {
+            memset(weights, 0, d * sizeof(float));
+        }
         memset(ys1, 0, n * d * sizeof(float));
         converged = true;
-
         for (int i = 0; i < n; i++) {
             float y1_norm = 0;
             for (int j = search_result.lims[i]; j < search_result.lims[i + 1]; j++) {
@@ -101,21 +108,25 @@ void MeanShiftClustering::train() {
             float shift = 0;
             for (int l = 0; l < d; l++) {
                 ys1[i * d + l] /= y1_norm;
-                float disp = xs[i * d + l] - ys[i * d + l];
-                weights[l] += disp * disp;
+                if (weighted) {
+                    float disp = xs[i * d + l] - ys[i * d + l];
+                    weights[l] += disp * disp;
+                }
                 float shift_d = ys1[i * d + l] - ys[i * d + l];
                 shift += shift_d * shift_d;
             }
             shift = sqrt(shift);
             converged &= shift < tolerance;
         }
-        float weight_norm = 0;
-        for (int l = 0; l < d; l++) {
-            weights[l] = exp(-weights[l] / n / lambda);
-            weight_norm += weights[l];
-        }
-        for (int l = 0; l < d; l++) {
-            weights[l] /= weight_norm;
+        if (weighted) {
+            float weight_norm = 0;
+            for (int l = 0; l < d; l++) {
+                weights[l] = exp(-weights[l] / n / lambda);
+                weight_norm += weights[l];
+            }
+            for (int l = 0; l < d; l++) {
+                weights[l] /= weight_norm;
+            }
         }
 
         float* temp = ys;
--- faiss/MeanShiftClustering.h
+++ faiss/MeanShiftClustering.h
@@ -35,9 +35,11 @@ struct MeanShiftClustering {
     /// entropy regularization coefficient for weight update
     float lambda = 10.;
     /// stop when the mean shift magnitude is less than tolerance for all points
-    float tolerance = 1e-6;
+    float tolerance = 1e-4;
     /// distance threshold for cluster extraction via connected components (should be larger than threshold)
-    float epsilon = 1e-5;
+    float epsilon = 1e-3;
+    /// do weighted blurring mean shift
+    bool weighted = false;
     /// log info during clustering
     bool verbose = false;
 
