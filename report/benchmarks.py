ec3437c4c4a24596b2f00bd4c0a9268784c34f87
Arti Schmidt <artischmidt3@gmail.com>
2023-12-08 21:03:35 -0500

    Finish visualization script

--- report/benchmarks.py
+++ report/benchmarks.py
@@ -1,32 +1,42 @@
 import os.path
 import clustbench
-import faiss
+from sklearn.cluster import estimate_bandwidth
+import seaborn.objects as so
 import numpy as np
-import sklearn.cluster
-import seaborn as sns
-import matplotlib.pyplot as plt
 from wrapper import MeanShift
 
 
 path = os.path.join('~', 'clustering-data-v1')
-dataset = clustbench.load_dataset('wut', 'x1', path=path)
+dataset = clustbench.load_dataset('wut', 'x3', path=path)
+rng = np.random.default_rng()
+X = rng.choice(dataset.data, size=100)
+# X = dataset.data
 
-mean_shift = MeanShift(dataset.data)
+bandwidth = estimate_bandwidth(X)
+mean_shift = MeanShift(X, bandwidth=bandwidth)
 labels, centroids, Ys = mean_shift.run()
 
-labels += 1 # clustbench requires labels between 1 and k
-k_pred = centroids.shape[0]
-assert k_pred in dataset.n_clusters # scoring will error if k_pred is not k_true in any of the reference clusterings
+# labels += 1 # clustbench requires labels between 1 and k
+# k_pred = centroids.shape[0]
+# assert k_pred in dataset.n_clusters # scoring will error if k_pred is not k_true in any of the reference clusterings
+# score = clustbench.get_score(dataset.labels, labels)
 
-score = clustbench.get_score(dataset.labels, labels)
-
-# print(k_pred)
-# print(labels)
-# print(centroids)
-# print(Ys)
-# print(score)
-
+data = {
+    'iter': [],
+    'x': [],
+    'y': [],
+    'label': [],
+}
 for i in range(Ys.shape[0]):
-    sns.set_theme()
-    sns.scatterplot(x=Ys[i,:,0], y=Ys[i,:,1], hue=labels)
-    plt.show()
+    for j in range(Ys.shape[1]):
+        data['iter'].append(i)
+        data['x'].append(Ys[i,j,0])
+        data['y'].append(Ys[i,j,1])
+        data['label'].append(labels[j])
+
+(so.Plot(data, x='x', y='y', color='label')
+.facet('iter', wrap=4)
+.add(so.Dot(), legend=False)
+.label(x='', y='', col='Iteration')
+.theme({'axes.facecolor': '#ffffff', 'axes.edgecolor': '#000000'})
+.show())
